6992424d6a202e9b89cbbc970e622cec493c00df
thanhvinh-htnbt <147599687+thanhvinh-htnbt@users.noreply.github.com>
2023-11-23 17:51:47 +0700

    Add files via upload

---
+++ printint.c
@@ -0,0 +1,11 @@
+#include "syscall.h" 
+#include "copyright.h" 
+#define maxlen 32 
+
+int main() 
+{ 
+	printf("\n Nhap so:");
+	int number=ReadInt();
+	printf("\n So da nhap la: %d", number);
+	return 0;
+}
